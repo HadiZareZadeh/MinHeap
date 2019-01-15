c20e45432dbc6b4c6c99684418d2701a72bf4fe1
ashkan1996 <zarehadi2@gmail.com>
2019-01-16 00:31:03 +0330

    Add files via upload

---
+++ MinHeap - SJ/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MinHeap___SJ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(479, 328);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(40, 20);
+            this.txtNumber.TabIndex = 0;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(525, 326);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(47, 23);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.Enter_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(5, 315);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(468, 42);
+            this.txtLog.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtNumber);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ MinHeap - SJ/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinHeap___SJ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = CreateGraphics();
+        }
+        Graphics graphics;
+
+        int[] array = new int[1000];
+        int n = 0;
+
+        private void MakeMinHeap(int[] a, int n, int i)
+        {
+            int s = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+            if (l < n && a[l] > a[s])
+                s = l;
+            if (r < n && a[r] > a[s])
+                s = r;
+            if (s != i)
+            {
+                int temp = a[i];
+                a[i] = a[s];
+                a[s] = temp;
+                MakeMinHeap(a, n, s);
+            }
+        }
+        
+        private void heapSort(int[] a, int n)
+        {
+            for (int i = (n / 2) - 1; i >= 0; i--)
+                MakeMinHeap(a, n, i);
+            
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = a[0];
+                a[0] = a[i];
+                a[i] = temp;
+                MakeMinHeap(a, i, 0);
+            }
+        }
+
+        List<PointF> points;
+        int x, y;
+        private void PrintGraph()
+        {
+            points = new List<PointF>();
+            int b = 1;
+            int i = 0;
+            while(i < n)
+            {
+                for (int j = 0; j < b && i < n; j++, i++)
+                {
+                    graphics.FillEllipse(Brushes.LightSeaGreen, x + (2 * x * j) - (10), y, 20, 20);
+                    graphics.DrawEllipse(Pens.Black, x + (2 * x * j) - (10), y, 20, 20);
+                    graphics.DrawString(array[i].ToString(), Font, Brushes.Black, x + (2 * x * j) + 6 - (10), y + 6);
+                    points.Add(new PointF(x + (2 * x * j) - (10), y));
+                }
+                b *= 2;
+                y += 40;
+                x /= 2;
+            }
+
+            for(i = 1; i < n; i++)
+            {
+                PointF p1 = points[(i - 1) / 2];
+                PointF p2 = points[i];
+
+                p1.Y += 20;
+                p1.X += 10;
+
+                p2.X += 10;
+
+                graphics.DrawLine(Pens.Black, p1, p2);
+            }
+        }
+
+        private void Log()
+        {
+            txtLog.Clear();
+            for(int i = 0; i < n; i++)
+            {
+                txtLog.Text += array[i] + " ";
+            }
+        }
+
+        private void Enter_Click(object sender, EventArgs e)
+        {
+            x = Width / 2;
+            y = 10;
+            int number = Convert.ToInt32(txtNumber.Text);
+            array[n] = number;
+            n++;
+            heapSort(array, n);
+            Log();
+            graphics.Clear(BackColor);
+            PrintGraph();
+            txtNumber.Clear();
+        }
+    }
+}
